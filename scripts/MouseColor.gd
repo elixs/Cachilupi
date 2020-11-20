092c34ec6c1455aec95bf84ce04d60b4b78cd394
Elías Zelada <elixs.777@gmail.com>
2020-11-20 11:41:24 -0300

    Update MouseColor.gd

--- scripts/MouseColor.gd
+++ scripts/MouseColor.gd
@@ -1,9 +1,5 @@
 extends ColorRect
 
-
-onready var viewport_rect = get_viewport_rect()
-
-
 func _process(delta: float) -> void:
 	# TODO make this support window resize
 	var pos = 2 * get_global_mouse_position() / get_viewport().size
